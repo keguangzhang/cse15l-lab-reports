15608c9f8b51726a99e4d6e151c576e347123281
keguangzhang <kez013@ucsd.edu>
2023-01-30 14:03:44 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,27 @@
+import java.net.URI;
+
+
+class Handler {
+    public void handleRequest(URI url) {
+        if (url.getPath().contains("/add-message")) {
+            String[] strings = url.getQuery().split("=");
+            /* 
+            String stringToReturn = strings[1];
+            return stringToReturn;
+            */
+            for (int i = 0; i < strings.length; i++) {
+                System.out.println(strings[i]);
+            }
+        }
+            
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) {
+        Handler request = new Handler();
+
+        request.handleRequest("/add-message?s=Hello");
+    }
+
+}
